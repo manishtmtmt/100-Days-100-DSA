93922ad63bbd3d16bfcf04d97292f0699c3dea56
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-07-04 23:33:58 +0530

    single number II js solution

--- Day-1/solution.js
+++ Day-1/solution.js
@@ -2,4 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function (nums) {};
+var singleNumber = function (nums) {
+  let obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (obj[nums[i]] === undefined) {
+      obj[nums[i]] = 1;
+    } else {
+      obj[nums[i]]++;
+    }
+  }
+
+  for (let key in obj) {
+    if (obj[key] === 1) return key;
+  }
+};
